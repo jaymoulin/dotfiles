fb5a6da8043bf7ed6d294a0a1a137eaa6aeab4d0
Jeremy MOULIN <jeremy.moulin@twenga.com>
2017-01-26 15:37:08 +0100

    add rpi alias

--- aliases.sh
+++ aliases.sh
@@ -6,6 +6,7 @@ echo "alias gif='git links'" >> ~/.bash_aliases
 echo "alias giz='git z'" >> ~/.bash_aliases
 echo "alias gam='git ac'" >> ~/.bash_aliases
 echo "alias gitclean='git tidy'" >> ~/.bash_aliases
+echo "alias rpi='ssh femtopixel.hd.free.fr'" >> ~/.bash_aliases
 
 echo "search() {
   find . -type f -exec grep -H \"\$1\" {} \; | grep \"\$1\"
