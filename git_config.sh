d48d7831f8009a972ddb3c92eacbcc163759e981
jmoulin <jmoulin@castelis.com>
2017-03-27 12:23:43 +0200

    Fix git config install for gitignore wherever we are

--- git_config.sh
+++ git_config.sh
@@ -2,6 +2,9 @@
 
 echo "\033[1;31mInstalling Git Config...\033[0m"
 
+
+DIR="$(cd $( dirname $0) && pwd)"
+
 git config --global user.name "Jay MOULIN"
 git config --global user.email "jaymoulin@gmail.com"
 git config --global core.excludesfile ~/.gitignore
@@ -9,4 +12,4 @@ git config --global core.autocrlf input
 git config --global core.eol lf
 git config --global core.editor vim
 git config --global push.default simple
-cp ./gitconfig/.gitignore ~/.gitignore
+cp $DIR/gitconfig/.gitignore ~/.gitignore
