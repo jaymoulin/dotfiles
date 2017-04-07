3cbada978ada8e40d1642d844e9b8a71b4a1b925
jmoulin <jmoulin@castelis.com>
2017-04-07 16:08:25 +0200

    Move liquidprompt to hidden directory

--- prompt.sh
+++ prompt.sh
@@ -2,7 +2,6 @@
 
 echo "\033[1;31mInstalling Prompt...\033[0m"
 
-cd
-git clone https://github.com/nojhan/liquidprompt.git
+git clone https://github.com/nojhan/liquidprompt.git ~/.liquidprompt
 
-echo "[[ \$- = *i* ]] && source ~/liquidprompt/liquidprompt" >> ~/.bashrc
+echo "[[ \$- = *i* ]] && source ~/.liquidprompt/liquidprompt" >> ~/.bashrc
