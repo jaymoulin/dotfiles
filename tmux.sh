ccd1dbdeff56650b35ecc8673167ab2335114f0c
jmoulin <jmoulin@castelis.com>
2017-04-24 14:02:41 +0200

    Split tools tmux and VIM

---
+++ tmux.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "\033[1;31mInstalling tmux...\033[0m"
+
+sudo apt-get install -y tmux
--- vim.sh
+++ vim.sh
@@ -2,5 +2,6 @@
 
 echo "\033[1;31mInstalling VIM...\033[0m"
 
+sudo apt-get install -y vim-nox
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
