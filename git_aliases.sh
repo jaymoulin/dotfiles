db045f603a0ce4853a0b736156142f1d33753bb4
jmoulin <jmoulin@castelis.com>
2017-09-13 12:18:39 +0200

    rename plup to up

--- git_aliases.sh
+++ git_aliases.sh
@@ -49,4 +49,4 @@ git config --global alias.ac '!git add --all && git amend'
 git config --global alias.vanish '!git checkout master && git links && git branch -r --merged | grep -v "origin/master" | grep -v "origin/HEAD" | xargs -I "{}" echo "{}" | sed "s/^origin\//origin \:/g" | xargs -I "{}" echo "git push {}" | bash - && git tidy'
 
 #Pull current branch with rebasing method
-git config --global alias.plup '!git fetch && git rebase @{u}'
+git config --global alias.up '!git fetch && git rebase @{u}'
