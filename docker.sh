63da6c942e0b0c9e19d4eb80bafb784616f7aca0
Jay MOULIN <jmoulin@castelis.com>
2017-11-20 12:23:15 +0100

    added multiarch system

--- docker.sh
+++ docker.sh
@@ -7,3 +7,7 @@ sudo apt-get install curl python3-pip -y && sudo curl -sSL https://get.docker.co
 echo "\033[1;31mInstalling Docker - Manifest-Tool...\033[0m"
 git clone https://github.com/estesp/manifest-tool && cd manifest-tool && sudo make && sudo make install && cd .. && \
  rm -Rf manifest-tool
+
+echo "\033[1;31mInstalling Docker - Multiarch...\033[0m"
+docker run --rm --privileged multiarch/qemu-user-static:register
+sudo apt install -y qemu qemu-user-static qemu-user bin   fmt-support
