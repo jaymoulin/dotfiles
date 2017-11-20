077b1b32bab693a1d64214ed87c5dc35e48d9117
Jay MOULIN <jmoulin@castelis.com>
2017-11-20 12:01:06 +0100

    Added docker install

---
+++ docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "\033[1;31mInstalling Docker\033[0m"
+sudo apt-get install curl python3-pip -y && sudo curl -sSL https://get.docker.com | sudo sh && \
+    sudo usermod -aG docker $USER && sudo pip3 install docker-compose
+
+echo "\033[1;31mInstalling Docker - Manifest-Tool...\033[0m"
+git clone https://github.com/estesp/manifest-tool && cd manifest-tool && sudo make && sudo make install && cd .. && \
+ rm -Rf manifest-tool
