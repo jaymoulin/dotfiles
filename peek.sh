bba65fe9dc147b16bf2268b00c58e66065b0992c
Jay MOULIN <jmoulin@castelis.com>
2017-11-30 11:28:13 +0100

    Added asciinema

--- peek.sh
+++ peek.sh
@@ -4,3 +4,7 @@ echo "\033[1;31mInstalling Peek (screen2gif)...\033[0m"
 
 sudo apt update && sudo apt install peek
 
+
+echo "\033[1;31mInstalling Asciinema...\033[0m"
+
+sudo pip3 install asciinema
