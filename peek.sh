69950c7602a4d1084e2eae5c8b3b0d5a7600542d
Jay MOULIN <jaymoulin@gmail.com>
2017-12-23 00:52:10 +0100

    May fix peek install

--- peek.sh
+++ peek.sh
@@ -2,9 +2,9 @@
 
 echo "\033[1;31mInstalling Peek (screen2gif)...\033[0m"
 
-sudo apt update && sudo apt install peek
+sudo snap install peek
 
 
 echo "\033[1;31mInstalling Asciinema...\033[0m"
 
-sudo pip3 install asciinema
+sudo -H pip3 install asciinema
