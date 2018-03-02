149ff257b981b657592598d1687104399e5f9008
Jay MOULIN <jaymoulin@gmail.com>
2018-03-02 18:06:08 +0100

    added docker completions

--- macos/completions.sh
+++ macos/completions.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-echo -e "\033[1;31mInstalling GIT completions...\033[0m"
+echo -e "\033[1;31mInstalling completions...\033[0m"
 
 brew install git bash-completion
+sudo mkdir -p /etc/bash_completion.d/
+curl -L https://raw.githubusercontent.com/docker/compose/1.19.0/contrib/completion/bash/docker-compose -o $(brew --prefix)/etc/bash_completion.d/docker-compose
+curl -XGET https://raw.githubusercontent.com/docker/cli/master/contrib/completion/bash/docker > $(brew --prefix)/etc/bash_completion.d/docker
