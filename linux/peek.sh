d417c7423e990acdde39d375a0c6be2bea17f03b
Jay MOULIN <jaymoulin@gmail.com>
2018-05-31 16:57:35 +0200

    fixes peek install for linux

--- linux/peek.sh
+++ linux/peek.sh
@@ -2,7 +2,9 @@
 
 echo -e "\033[1;31mInstalling Peek (screen2gif)...\033[0m"
 
-sudo apt update && sudo apt install snap -y && sudo snap install peek
+
+sudo add-apt-repository ppa:peek-developers/stable
+sudo apt update && sudo apt install peek -y
 
 
 echo -e "\033[1;31mInstalling Asciinema...\033[0m"
