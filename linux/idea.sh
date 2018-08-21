e3becb2a426948202b8aa0e8dc91ec782aead485
Jay MOULIN <jeremy@weenect.com>
2018-08-21 10:26:38 +0200

    patches idea to avoid license issues

--- linux/idea.sh
+++ linux/idea.sh
@@ -2,3 +2,4 @@
 
 echo -e "\033[1;31mInstalling IDEA\033[0m"
 sudo apt-get install snap -y && sudo snap install intellij-idea-ultimate --classic --edge
+echo "0.0.0.0 account.jetbrains.com" | sudo tee -a /etc/hosts
