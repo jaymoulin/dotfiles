b5398394f039824e73a9ddaf6df60c8468089268
Jay MOULIN <jaymoulin@gmail.com>
2018-11-11 17:42:52 +0100

    no Docker multiarch for now - this kills all bin links

--- linux/docker.sh
+++ linux/docker.sh
@@ -8,12 +8,12 @@ echo -e "\033[1;31mInstalling Docker - Manifest-Tool...\033[0m"
 git clone https://github.com/estesp/manifest-tool && cd manifest-tool && sudo make && sudo make install && cd .. && \
  rm -Rf manifest-tool
 
-echo -e "\033[1;31mInstalling Docker - Multiarch...\033[0m"
-sudo apt install -y qemu qemu-user-static qemu-user binfmt-support
-docker run --rm --privileged multiarch/qemu-user-static:register
+#echo -e "\033[1;31mInstalling Docker - Multiarch...\033[0m"
+#sudo apt install -y qemu qemu-user-static qemu-user binfmt-support
+#docker run --rm --privileged multiarch/qemu-user-static:register
 
-git clone https://github.com/computermouth/qemu-static-conf.git
-sudo mkdir -p /lib/binfmt.d
-sudo cp qemu-static-conf/*.conf /lib/binfmt.d/
-sudo systemctl restart systemd-binfmt.service
-rm -Rf qemu-static-conf
+#git clone https://github.com/computermouth/qemu-static-conf.git
+#sudo mkdir -p /lib/binfmt.d
+#sudo cp qemu-static-conf/*.conf /lib/binfmt.d/
+#sudo systemctl restart systemd-binfmt.service
+#rm -Rf qemu-static-conf
