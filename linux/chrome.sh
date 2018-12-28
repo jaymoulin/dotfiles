002f3473318ed34a504d765e635a6351767f6375
Jay MOULIN <jaymoulin@gmail.com>
2018-12-28 19:15:54 +0100

    add chrome

---
+++ linux/chrome.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo -e "\033[1;31mInstalling chrome...\033[0m"
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable -y
