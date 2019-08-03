d3afd1cf5b3b49499818209061932f4f6546a015
Jay MOULIN <jaymoulin@gmail.com>
2019-08-03 07:11:45 +0200

    Fixes unity for disco dingo

--- linux/interface.sh
+++ linux/interface.sh
@@ -2,4 +2,4 @@
 
 echo -e "\033[1;31mInstalling unity...\033[0m"
 
-sudo apt install unity-session xserver-xorg-input-synaptics -y
+sudo apt install unity-session xserver-xorg-input-synaptics ubuntu-unity-desktop -y
