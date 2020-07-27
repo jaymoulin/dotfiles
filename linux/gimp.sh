95b102c15d21cc226e9bf5815ed77cf14dbdcaf6
Jay MOULIN <jaymoulin@gmail.com>
2020-07-27 03:24:08 +0200

    changes gimp install for snap

--- linux/gimp.sh
+++ linux/gimp.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 echo -e "\033[1;31mInstalling gimp...\033[0m"
-sudo add-apt-repository ppa:otto-kesselgulasch/gimp
-sudo apt-get update
-sudo apt-get install -y gimp
+sudo apt-get install snap -y && sudo snap install gimp
