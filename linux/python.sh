f72e972c61877133b840a83217d7999ebcfc062e
Jay MOULIN <jaymoulin@gmail.com>
2020-08-17 08:03:13 +0200

    may fixes pip install

--- linux/python.sh
+++ linux/python.sh
@@ -4,8 +4,7 @@ echo -e "\033[1;31mInstalling Python...\033[0m"
 
 sudo apt-get remove python3-pip -y
 sudo apt-get install python3-pip -y
-sudo -H easy_install3 pip
-sudo -H pip install --upgrade pip
+sudo -H pip3 install --upgrade pip
 
 DIR="$(cd $( dirname $0) && pwd)"
 
