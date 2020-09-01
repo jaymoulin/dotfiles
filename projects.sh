baa4305c95d1d398caef57c4ae3c56f63fe31dc9
Jay MOULIN <jaymoulin@gmail.com>
2020-09-01 20:19:00 +0200

    Adds youtube-music-auth to projects

--- projects.sh
+++ projects.sh
@@ -22,6 +22,7 @@ github=(
     git@github.com:jaymoulin/google-music-manager-downloader
     git@github.com:jaymoulin/google-music-manager-auth
     git@github.com:jaymoulin/youtube_music_uploader.git
+    git@github.com:jaymoulin/youtube_music_auth.git
     git@github.com:femtopixel/docker-google-lighthouse-puppeteer
     git@github.com:femtopixel/google-lighthouse-puppeteer
     git@github.com:femtopixel/google-lighthouse-puppeteer-cli-dashboard
