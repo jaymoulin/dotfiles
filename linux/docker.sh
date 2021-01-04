7dbc743bb7f9f7889d61b40a2c49ecd23e1637f2
Jay MOULIN <jaymoulin@gmail.com>
2021-01-05 00:15:10 +0100

    do not install binmft docker multiarch as it crashes everything

--- linux/docker.sh
+++ linux/docker.sh
@@ -19,13 +19,3 @@ echo -e "/var/lib/docker/containers/*/*.log {
 }" | sudo tee --append /etc/logrotate.d/docker-container
 
 sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
-
-echo -e "\033[1;31mInstalling Docker - Multiarch...\033[0m"
-sudo apt install -y qemu qemu-user-static qemu-user binfmt-support
-docker run --rm --privileged multiarch/qemu-user-static:register
-
-git clone https://github.com/computermouth/qemu-static-conf.git
-sudo mkdir -p /lib/binfmt.d
-sudo cp qemu-static-conf/*.conf /lib/binfmt.d/
-sudo systemctl restart systemd-binfmt.service
-rm -Rf qemu-static-conf
