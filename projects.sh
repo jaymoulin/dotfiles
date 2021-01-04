f275e787ca3172c35f0de17cadd98e4a3ca12ff7
Jay MOULIN <jaymoulin@gmail.com>
2021-01-04 23:29:14 +0100

    removes old projects

--- projects.sh
+++ projects.sh
@@ -7,20 +7,15 @@ mkdir -p ~/projects/{jaymoulin,femtopixel}
 github=(
     git@github.com:jaymoulin/docker-plex
     git@github.com:jaymoulin/docker-jdownloader
-    git@github.com:jaymoulin/guzzlehttp-cloudflare
     git@github.com:jaymoulin/yamete
     git@github.com:jaymoulin/google-musicmanager-dedup-api
     git@github.com:jaymoulin/docker-twitter-cli
     git@github.com:jaymoulin/docker-twidge
     git@github.com:jaymoulin/docker-rpi-plex-video-converter
-    git@github.com:jaymoulin/docker-google-cloudprint
     git@github.com:jaymoulin/docker-coinmon
     git@github.com:jaymoulin/docker-sshtron
     git@github.com:jaymoulin/docker-piu-piu
     git@github.com:jaymoulin/docker-google-chrome-webstore-download
-    git@github.com:jaymoulin/google-music-manager-uploader
-    git@github.com:jaymoulin/google-music-manager-downloader
-    git@github.com:jaymoulin/google-music-manager-auth
     git@github.com:jaymoulin/youtube-music-uploader.git
     git@github.com:jaymoulin/kbueno-bot.git
     git@github.com:femtopixel/docker-google-lighthouse-puppeteer
@@ -33,7 +28,6 @@ github=(
     git@github.com:femtopixel/nano-framework
     git@github.com:femtopixel/monolog-csvhandler
     git@github.com:femtopixel/docker-pdf2gdocs
-    git@github.com:femtopixel/docker-google-closure-compiler-app
     git@github.com:femtopixel/crop
     git@github.com:femtopixel/github-release-notifier
     git@github.com:femtopixel/docker-google-closure-compiler-api
@@ -55,11 +49,8 @@ gitlab=(
     git@gitlab.com:jaymoulin/docker-autobuilder
     git@gitlab.com:jaymoulin/keeptune
     git@gitlab.com:jaymoulin/docker-transmission
-    git@gitlab.com:jaymoulin/docker-google-music-uploader
     git@gitlab.com:jaymoulin/mop-rpi
-    git@gitlab.com:jaymoulin/docker-google-musicmanager
     git@gitlab.com:jaymoulin/docker-googler
-    git@gitlab.com:jaymoulin/docker-google-music-downloader
     git@gitlab.com:jaymoulin/docker-ddgr
 )
 
