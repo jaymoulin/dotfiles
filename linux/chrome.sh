0b6d947d5877b6ddee990ea1845b6c556fad8e17
Jay MOULIN <jaymoulin@gmail.com>
2024-05-23 08:06:45 +0200

    Update chrome installation for ubuntu

--- linux/chrome.sh
+++ linux/chrome.sh
@@ -2,7 +2,8 @@
 
 echo -e "\033[1;31mInstalling chrome...\033[0m"
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable -y
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub > linux_signing_key.pub
+sudo install -D -o root -g root -m 644 linux_signing_key.pub /etc/apt/keyrings/linux_signing_key.pub
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/linux_signing_key.pub] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+sudo apt update
+sudo apt install google-chrome-stable -y
